6994a779bfc75f2dc7214c010e37725fdbe4d069
Evan Zhang <zhangevan82@gmail.com>
2024-06-13 16:45:55 -0400

    made the auth page where its both a signup and a login page has bugs where it can't compare with the database successfully.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import About from './pages/About';
 import Signup from './pages/Signup';
-import Login from './pages/Login';
+import Login from './pages/Auth';
 
 function App() {
   return (
